2dff517d4ffaefd2e9395a54698beb6ab5181291
rarrara99 <tiavn79@naver.com>
2020-11-24 00:29:29 +0900

    메인 테스트 코드

--- a0_Project/src/main/java/com/example/demo/controller/MainController.java
+++ a0_Project/src/main/java/com/example/demo/controller/MainController.java
@@ -2,19 +2,31 @@ package com.example.demo.controller;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+/*@Controller
 @EnableAutoConfiguration
 public class MainController {
 	@RequestMapping(value = "/")
 	//@ResponseBody
 	public String hello() { 
-		System.out.println("메인 페이지");
+		System.out.println("메인 페이지" );
 		//test1122aa 
 		
 		
 		return "test";
 	}
+}*/
+
+
+@RestController
+public class MainController {
+	@GetMapping ("/hello") 
+	public String hello() {
+		return "hello";
+	}
 }
+
